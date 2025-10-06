413a23074df732b8f36da1f58fce0c59c1b6f673
kazu1999 <squid.7171@gmail.com>
2025-10-06 14:51:19 +0900

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
           '/api': {
             target: proxyTarget,
             changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
+            rewrite: (path: string) => path.replace(/^\/api/, ''),
           },
         },
       }
